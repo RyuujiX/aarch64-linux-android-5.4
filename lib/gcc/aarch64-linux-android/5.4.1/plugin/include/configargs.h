49f9e83fc6e0dcf0beff52093067d6f77b53f8bc
Jacob McSwain <jacob.a.mcswain@gmail.com>
2016-06-10 18:34:18 -0500

    GCC 5.4.1 built on 10 June 2016

---
+++ lib/gcc/aarch64-linux-android/5.4.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-upstream/configure --prefix=/root/aarch-gcc5.4 --target=aarch64-linux-android --enable-languages=c,c++ --disable-multilib --with-gnu-as --with-newlib : (reconfigured) ../gcc-upstream/configure --prefix=/root/aarch-gcc5.4 --target=aarch64-linux-android --enable-languages=c,c++ --disable-multilib --with-gnu-as --disable-threads --with-newlib";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
